f53d08da0f1a689432e5f1748dbb5859c05cfd86
DeviaWindhede <devia.windheed@gmail.com>
2024-03-12 11:03:09 +0100

    added scroll event testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,36 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio';
+
+  new_scroll_position: number = 0;
+  last_scroll_position: number = 0;
+
+  constructor() {}
+  
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll(event: any) {
+    var header = document.getElementById("js-top");
+    
+    this.last_scroll_position = window.scrollY;
+
+    // Scrolling down
+    if (this.new_scroll_position < this.last_scroll_position && this.last_scroll_position > 184) {
+      // header?.classList.remove("is-visible");
+      // header?.classList.add("is-hidden");
+      
+      header?.classList.remove("is-hidden");
+      header?.classList.add("is-visible");
+
+      // Scrolling up
+    } else if (this.new_scroll_position > this.last_scroll_position) {
+      header?.classList.remove("is-hidden");
+      header?.classList.add("is-visible");
+    }
+
+    if (this.last_scroll_position < 184) {
+      header?.classList.remove("is-visible");
+    }
+
+    this.new_scroll_position = this.last_scroll_position;
+  }
 }
