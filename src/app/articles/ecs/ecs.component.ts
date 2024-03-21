7ff24caa09d22d56199e826981f658a51c6d5948
DeviaWindhede <devia.windheed@gmail.com>
2024-03-21 11:17:43 +0100

    Added some content to the ECS page

--- src/app/articles/ecs/ecs.component.ts
+++ src/app/articles/ecs/ecs.component.ts
@@ -10,42 +10,61 @@ import { RouterModule } from '@angular/router';
 })
 export class EcsComponent {
   readonly codeContainer = [
-    `
-    void someFunc() {
+// Component ID showcase
+`// Component.h
+template <typename Derived>
+struct Component
+{
+  friend class ComponentManager;
+private:
+  static cid_t componentId;
+};
 
-    }
-    `,
-    `
-    typedef unsigned long int cid_t;
+template <typename Derived>
+cid_t Component<Derived>::componentId = 0;
+`,
+`// ComponentManager.h
+class ComponentManager
+{
+public:
+	template<typename T>
+	void RegisterComponent(size_t aMaxSize)
+	{
+		T::componentId = myComponentContainers.size();
+		myComponentContainers.push_back(new ComponentContainer<T>(aMaxSize));
+	}
+private:
+  template<typename T>
+  ComponentContainer<T>* GetComponentContainer() const
+  {
+    cid_t cid = T::componentId;
+    assert(cid < myNextComponentId && "Component not registered before use.");
+    return (ComponentContainer<T>*)myComponentContainers[cid];
+  }
+}`,
 
-    template<typename T>
-    class ComponentContainer;
 
-    template <typename Derived>
-    struct Component
-    {
-    private:
-      friend class ComponentManager;
-      friend class ComponentContainer<Derived>;
-      static cid_t componentId;
-    public:
-      Component() = default;
-    private:
-      Component(const Component& aComponent) = default;
-      Component& operator=(const Component& aComponent) = default;
-    };
+// Component ID usage showcase
+`class ComponentManager
+{
+public:
+	template<typename T>
+	T& GetComponent(const Entity& aEntity) const
+	{
+		assert(myEntitySignatureCollection.GetComponentSignature(aEntity).test(T::componentId) && "Entity does not exist in ComponentContainer");
+		return GetComponentContainer<T>()->GetComponent(myEntitySignatureCollection.GetSignatureIndex(aEntity, T::componentId));
+	}
+private:
+	template<typename T>
+	ComponentContainer<T>* GetComponentContainer() const
+	{
+		cid_t cid = T::componentId;
 
-    template <typename Derived>
-    cid_t Component<Derived>::componentId = 0;
-    `,
-    `
-    class EntityManager
-    {
-      void Reset();
-      Entity CreateEntity();
-      Entity CreateEntityAtID(const Entity aEntity);
-      void DestroyEntity(const Entity aEntity);
-    }
-    `
-  ];
+		assert(cid < myNextComponentId && "Component not registered before use.");
+
+		return static_cast<ComponentContainer<T>*>(myComponentContainers.at(cid));
+	}
+};
+`
+];
 }
