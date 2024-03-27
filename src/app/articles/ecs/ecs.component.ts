9ed4e4ca2031fba73e1fa9e3803c41134e527af5
Devia Windhede <devia.windhede@gmail.com>
2024-03-27 15:09:16 +0100

    added code snippets

--- src/app/articles/ecs/ecs.component.ts
+++ src/app/articles/ecs/ecs.component.ts
@@ -11,7 +11,91 @@ import { BackToTopBtnComponent } from '../../utility/back-to-top-btn/back-to-top
   styleUrl: './ecs.component.scss'
 })
 export class EcsComponent {
-  readonly codeContainer = [
+	readonly entityManager: string =
+`class EntityManager
+{
+public:
+	EntityManager();
+	~EntityManager();
+
+	void Reset();
+	Entity CreateEntity();
+	Entity CreateEntityAtID(const eid_t& aEntity);
+	void DestroyEntity(const Entity& aEntity);
+private:
+	std::deque<eid_t> myFreeEntities;
+`;
+	readonly componentManager: string =
+`class ComponentManager
+{
+public:
+	friend class World;
+
+	ComponentManager();
+	~ComponentManager();
+
+	void Reset();
+
+	template<typename T>
+	void RegisterComponent(size_t aMaxSize);
+
+	template<typename T>
+	T& AddComponent(const Entity& aEntity);
+
+	template<typename T>
+	void RemoveComponent(const Entity& aEntity);
+
+	template<typename T>
+	T& GetComponent(const Entity& aEntity) const;
+
+	void OnEntityDestroyed(const Entity& aEntity);
+
+	template<typename T>
+	bool HasComponent(const Entity& aEntity) const;
+
+	template<typename T>
+	void UpdateComponentSignature(const Entity& aEntity, bool aValue);
+	
+	const ComponentSignature& GetEntitySignature(const Entity& aEntity) const;
+private:
+	std::vector<IComponentContainer*> myComponentContainers;
+	EntitySignatureCollection myEntitySignatureCollection;
+
+	template<typename T>
+	ComponentContainer<T>* GetComponentContainer() const;
+};
+`;
+
+	readonly systemManager: string = 
+`
+class SystemManager
+{
+public:
+	SystemManager(World* aWorld);
+	~SystemManager();
+
+	void Reset();
+	void Update(SceneUpdateContext& aContext);
+	void Render();
+	void Init();
+
+	template<typename T, typename... Param>
+	T* RegisterSystem(Param... args);
+
+	template<typename T>
+	void SetSignature(const ComponentSignature& aSignature),
+
+	void OnEntityDestroyed(const Entity& aEntity);
+
+	void OnEntitySignatureChanged(const Entity& aEntity, const ComponentSignature& aSignature);
+private:
+	World* myWorld;
+
+	std::vector<ComponentSignature> mySystemSignatures; // used for filtering entities
+	std::vector<ISystem*> mySystems;
+`;
+
+  	readonly codeContainer = [
 // Component ID showcase
 `// Component.h
 template <typename Derived>
