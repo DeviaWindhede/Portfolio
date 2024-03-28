408e9f3fad36fab1516f5c5fd21ee60ebd6e80ed
DeviaWindhede <devia.windheed@gmail.com>
2024-03-28 12:41:48 +0100

    added vertex painting text and gifs, separated common grid from game projects

--- src/app/articles/vertex-painter/vertex-painter.component.ts
+++ src/app/articles/vertex-painter/vertex-painter.component.ts
@@ -32,4 +32,33 @@ export class VertexPainterComponent {
     │   yetAnotherMesh_0.dds
     │   vertexDefines.data
 `;
+
+readonly vertexShaderPart1 =`
+uint2 GetInitialUVIndexFromEntity(uint entityIndex, uint textureWidth, uint textureHeight)
+{
+    float4 vertexColors = aVertexColorTexture.Load(int3(0, 0, 0));
+    
+    uint vertCount = f2ui8(vertexColors.r);
+    vertCount |= f2ui8(vertexColors.g) << 8;
+    vertCount |= f2ui8(vertexColors.b) << 16;
+    vertCount |= f2ui8(vertexColors.a) << 24;
+    
+    uint i = vertCount * entityIndex + 1;
+    return uint2(i % textureWidth, i / textureHeight);
+}`; 
+
+readonly vertexShaderPart2 =`
+uint2 GetUVIndexFromEntityVertex(uint vertexIndex, uint entityIndex)
+{
+    uint width = 0;
+    uint height = 0;
+    uint levels = 0;
+    aVertexColorTexture.GetDimensions(0, width, height, levels);
+    
+    uint2 result = GetInitialUVIndexFromEntity(entityIndex, width, height);
+    result.x += uint(vertexIndex % width);
+    result.y += uint(vertexIndex / height);
+    
+    return result;
+}`; 
 }
