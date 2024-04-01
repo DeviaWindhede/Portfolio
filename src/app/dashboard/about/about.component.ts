d4f97325e1a5e212cc1d2400e1f7a0926267df2f
DeviaWindhede <devia.windheed@gmail.com>
2024-04-02 00:50:58 +0200

    further content changes

--- src/app/dashboard/about/about.component.ts
+++ src/app/dashboard/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { LazyImgDirective } from '../../lazy-img.directive';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [ LazyImgDirective, RouterOutlet ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
