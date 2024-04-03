f9b4cde43f6f88b714383abebb8a58675124b994
DeviaWindhede <devia.windheed@gmail.com>
2024-04-04 00:09:19 +0200

    finished vertex painter page

--- src/app/articles/vertex-painter/vertex-painter.component.ts
+++ src/app/articles/vertex-painter/vertex-painter.component.ts
@@ -12,60 +12,25 @@ import { NgxGistModule } from '@ekkolon/ngx-gist';
   styleUrl: './vertex-painter.component.scss'
 })
 export class VertexPainterComponent {
-  @ViewChild('selection') selectionElement!: ElementRef;
-
-  scrollToSelection(): void {
-    this.selectionElement.nativeElement.scrollIntoView();
+  scrollToElement(el: HTMLHeadElement): void {
+    el.scrollIntoView();
   }
 
-  readonly codeContainer = [
-    `void someFunc() {
-
-    }`,
-    'test2', 'test3', 'test4', 'test5'
-  ];
-
   readonly exampleFolderStructure = 
-`vertex-painter-dir
+`vertexTextures
 │
 └───sceneName0
 │   │   mesh_0.dds
 │   │   mesh_1.dds
 │   │   anotherMesh_0.dds
 │   │   vertexDefines.data
+│   │   vertexIndexDefines.data
 │   
 └───sceneName1
     │   mesh_0.dds
     │   yetAnotherMesh_0.dds
     │   vertexDefines.data
+    │   vertexIndexDefines.data
 `;
 
-readonly vertexShaderPart1 =`
-uint2 GetInitialUVIndexFromEntity(uint entityIndex, uint textureWidth, uint textureHeight)
-{
-    float4 vertexColors = aVertexColorTexture.Load(int3(0, 0, 0));
-    
-    uint vertCount = f2ui8(vertexColors.r);
-    vertCount |= f2ui8(vertexColors.g) << 8;
-    vertCount |= f2ui8(vertexColors.b) << 16;
-    vertCount |= f2ui8(vertexColors.a) << 24;
-    
-    uint i = vertCount * entityIndex + 1;
-    return uint2(i % textureWidth, i / textureHeight);
-}`; 
-
-readonly vertexShaderPart2 =`
-uint2 GetUVIndexFromEntityVertex(uint vertexIndex, uint entityIndex)
-{
-    uint width = 0;
-    uint height = 0;
-    uint levels = 0;
-    aVertexColorTexture.GetDimensions(0, width, height, levels);
-    
-    uint2 result = GetInitialUVIndexFromEntity(entityIndex, width, height);
-    result.x += uint(vertexIndex % width);
-    result.y += uint(vertexIndex / height);
-    
-    return result;
-}`; 
 }
