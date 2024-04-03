18e7211b10561f5a7905470469425585521900d4
DeviaWindhede <devia.windheed@gmail.com>
2024-04-04 00:22:40 +0200

    added gitignore and fixed broken gist

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4WSYGHME.mjs";import"./chunk-L3DDIK5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-LQS3SYLU.mjs";import"./chunk-L3DDIK5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
