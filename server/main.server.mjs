967a7e37aec55f607a34d5ced6e47e18e4b94733
DeviaWindhede <devia.windheed@gmail.com>
2024-04-04 22:13:48 +0200

    added shitbox framework

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LQS3SYLU.mjs";import"./chunk-L3DDIK5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-RAYN2EDV.mjs";import"./chunk-L3DDIK5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
