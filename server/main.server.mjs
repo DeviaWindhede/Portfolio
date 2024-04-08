fa7f1d1ee72304d78c7889a4c6e252500ed25853
DeviaWindhede <devia.windheed@gmail.com>
2024-04-08 20:47:31 +0200

    updates

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RAYN2EDV.mjs";import"./chunk-L3DDIK5O.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-VZWJ3IKQ.mjs";import"./chunk-MGLJJZTV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
