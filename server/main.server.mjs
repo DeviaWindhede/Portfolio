6b5d424083d20518e7e3cfa1264ab77c392a5647
DeviaWindhede <devia.windheed@gmail.com>
2024-04-09 16:44:03 +0200

    added dark theme and updated some content

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-VZWJ3IKQ.mjs";import"./chunk-MGLJJZTV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AVZ2WHQ2.mjs";import"./chunk-6XTDPFCJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
