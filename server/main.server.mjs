cd331854952174ab0bd18ad055af69cb0ce79c32
DeviaWindhede <devia.windheed@gmail.com>
2024-04-09 23:49:56 +0200

    added final changes pre launch

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AVZ2WHQ2.mjs";import"./chunk-6XTDPFCJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AP6CMML4.mjs";import"./chunk-6XTDPFCJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
