504239a7dce951bf4ca6a85f015be6487e3152de
DeviaWindhede <devia.windheed@gmail.com>
2024-04-10 00:07:25 +0200

    fixed mobile navbar styling

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AP6CMML4.mjs";import"./chunk-6XTDPFCJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-IDY2OKRA.mjs";import"./chunk-6XTDPFCJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
